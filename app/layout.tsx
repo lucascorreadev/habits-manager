a5797fc3a7e057f7d23f88a40f419483a085c98b
Correa <fclucascorrea@gmail.com>
2024-01-02 12:45:22 -0300

    [feat] main page structure done

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next'
+import { Dosis, Inter } from 'next/font/google'
+import './globals.css'
+import Image from 'next/image'
+
+const dosis = Dosis({ subsets: ['latin'], variable: "--font-dosis" })
+const inter = Inter({ subsets: ['latin'], variable: "--font-inter"  })
+
+
+export const metadata: Metadata = {
+  title: 'Meta Diária - Gerenciador de Hábitos',
+  description: 'Gerencie todos os seus hábitos',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${dosis.variable} ${inter.variable}
+      flex items-center flex-col mt-10 bg-neutral-900`}>
+      <Image src="/images/logo.svg" width={200} height={200}
+      alt="Logo - meta diária"/>
+      {children}
+      </body>
+    </html>
+  )
+}
